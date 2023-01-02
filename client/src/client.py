412c01518ea795b8ff2f0f3e21413f36a1178c89
kartof0nix <karol.bonatkarol@gmail.com>
2023-01-02 22:31:55 +0100

    Added SSL and certificate verification.

--- client/src/client.py
+++ client/src/client.py
@@ -1,11 +1,20 @@
 import socket
+import ssl
 
-HOST = "127.0.0.1"  # The server's hostname or IP address
-PORT = 65432  # The port used by the server
+context = ssl.create_default_context()
+context.load_verify_locations('server/res/cert/cert.pem')
+# print(context.get_ciphers())
+print("LOL")
+HOST = "localhost"  # The server's hostname or IP address
+PORT = 65433  # The port used by the server
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b"Hello, world")
-    data = s.recv(1024)
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+    sock.connect((HOST, PORT))
+
+    with context.wrap_socket(sock, server_hostname=HOST) as ssock:
+        print(ssock.version())
+    #ssock.connect((HOST, PORT))
+        ssock.send(b"Hello, world")
+        data = ssock.recv(1024)
 
 print(f"Received {data!r}")
--- server/src/server.py
+++ server/src/server.py
@@ -1,17 +1,33 @@
 import socket
+import ssl
 
-HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
-PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+HOST = "localhost"  # Standard loopback interface address (localhost)
+PORT = 65433  # Port to listen on (non-privileged ports are > 1023)
+
+context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+context.load_cert_chain('./server/res/cert/cert.pem', './server/res/cert/key.pem')
+
+# cipher = 'DHE-RSA-AES128-SHA:DHE-RSA-AES256-SHA:ECDHE-ECDSA-AES128-GCM-SHA256'
+# context.set_ciphers(cipher)
+
+#print(context.get_ciphers())
+
+print("redy..")
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
-    while True:
-        conn, addr = s.accept()
-        with conn:
-            print(f"Connected by {addr}")
-            while True:
-                data = conn.recv(1024)
-                if not data:
-                    break
-                conn.sendall(data)
+
+    with context.wrap_socket(s, server_side=True) as ssock:
+
+        while True:
+            conn, addr = ssock.accept()
+
+            with conn:
+                print(f"Connected by {addr}")
+                while True:
+                    data = conn.recv(1024)
+                    if not data:
+                        break
+                    conn.send(data)
+    s.close()
