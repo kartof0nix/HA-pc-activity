3cf52b813ac92ff96971c0a2996e3a91574a2bd1
kartof0nix <karol.bonatkarol@gmail.com>
2023-01-03 17:50:13 +0100

    Added simple ( token ) authentication and state-saving.

--- client/src/client.py
+++ client/src/client.py
@@ -1,20 +1,60 @@
 import socket
 import ssl
+import subprocess
+import time
+# import json
+
+pattern = {"wifi":"nmcli -g name connection show --active"}
+
+HOST = "localhost"  # The server's hostname or IP address
+PORT = 65439  # The port used by the server
+NAME="karol-hp"
+TOKEN="3gdf8ugg"
+
+SSL=True
+UPDATE_FRQ = 5
+
+def connect_and_send(data):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.connect((HOST, PORT))
+
+        if(ssl):
+            s = context.wrap_socket(sock, server_hostname=HOST)
+            print(s.version())
+        else:
+            s = sock
+
+        s.send(bytes(data, 'utf-8'))
+        rcv = s.recv(1024)
+        s.close()
+    return rcv
+
 
 context = ssl.create_default_context()
 context.load_verify_locations('server/res/cert/cert.pem')
-# print(context.get_ciphers())
-print("LOL")
-HOST = "localhost"  # The server's hostname or IP address
-PORT = 65433  # The port used by the server
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-    sock.connect((HOST, PORT))
 
-    with context.wrap_socket(sock, server_hostname=HOST) as ssock:
-        print(ssock.version())
-    #ssock.connect((HOST, PORT))
-        ssock.send(b"Hello, world")
-        data = ssock.recv(1024)
+while True:
+    payload = {"name" : NAME, "token" : TOKEN}
+    data = {}
+
+    for cm in pattern:
+        print(pattern[cm])
+        p = subprocess.run(pattern[cm].split(" "), capture_output=True)
+        out = p.stdout.decode('utf-8')
+        out = out.replace('\n', '') #Remove trailing newline
+        data |= {cm : out}
+
+
+    payload |= {"data" : data}
+
+    print(payload)
+
+    print(connect_and_send(payload.__str__().replace("'", '"')))
+
+    time.sleep(UPDATE_FRQ)
+
+
+
 
 print(f"Received {data!r}")
--- server/src/server.py
+++ server/src/server.py
@@ -1,33 +1,57 @@
 import socket
 import ssl
+import json
 
 HOST = "localhost"  # Standard loopback interface address (localhost)
-PORT = 65433  # Port to listen on (non-privileged ports are > 1023)
+PORT = 65439  # Port to listen on (non-privileged ports are > 1023)
+SSL=True
 
 context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
 context.load_cert_chain('./server/res/cert/cert.pem', './server/res/cert/key.pem')
 
+known_devices = {"karol-hp" : "3gdf8ug"}
+states = {}
 # cipher = 'DHE-RSA-AES128-SHA:DHE-RSA-AES256-SHA:ECDHE-ECDSA-AES128-GCM-SHA256'
 # context.set_ciphers(cipher)
 
 #print(context.get_ciphers())
 
 print("redy..")
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+    sock.bind((HOST, PORT))
+    sock.listen()
+    if(SSL):
+        s = context.wrap_socket(sock, server_side=True)
+    else:
+        s = sock
+    
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
+    while True:
+        conn, addr = s.accept()
 
-    with context.wrap_socket(s, server_side=True) as ssock:
+        with conn:
+            print(f"Connected by {addr}")
+            rcv = b""
+            while True:
+                d = conn.recv(1024)
+                if not d:
+                    break
+                rcv += d
+                conn.send(b"OK")
 
-        while True:
-            conn, addr = ssock.accept()
+        
+        data = json.loads(rcv.decode('utf-8'))
+        
+        if(data['name'] not in known_devices):
+            print("Unknown device!")
+            continue
+        if(data['token'] != known_devices[data["name"]]):
+            print("Invalid token!")
+            continue
+        
+        states[data['name']] = data['data']
 
-            with conn:
-                print(f"Connected by {addr}")
-                while True:
-                    data = conn.recv(1024)
-                    if not data:
-                        break
-                    conn.send(data)
+        print(states)
+            #print("p")
+
     s.close()
